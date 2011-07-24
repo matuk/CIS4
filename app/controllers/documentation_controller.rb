14853b3f5a0f93b826a5d71122d19ae1ed90be8c
mattuk <makampf@gmail.com>
2011-07-24 18:16:15 +0200

    Added a Documentation Controller

---
+++ app/controllers/documentation_controller.rb
@@ -0,0 +1,8 @@
+class DocumentationController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Cis4::Application.routes.draw do
+  get "documentation/home"
+
+  get "documentation/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
